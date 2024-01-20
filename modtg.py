54160820d8bbd2e6490a7e314e51bebce1e64cb8
Alexey <alexcheremisin@mail.ru>
2024-01-21 01:19:39 +0300

    add_mod

---
+++ modtg.py
@@ -0,0 +1,12 @@
+from datetime import datetime
+from datetime import date
+ourday = datetime(2020,1,25,23,59,59)
+today = datetime.now()
+diff=ourday-today
+ydiff=diff.days*-1
+hdiff=diff.seconds//3600
+mdiff=(diff.seconds%3600)//60
+sdiff=()
+
+
+print(ydiff,hdiff,mdiff)
