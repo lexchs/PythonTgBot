0668b3245c296725034055177a092d4a055e568e
lexchs <124810229+lexchs@users.noreply.github.com>
2024-01-21 18:37:20 +0300

    Update tbot.py

--- tbot.py
+++ tbot.py
@@ -1,3 +1,4 @@
+from modtg import ourdate
 import requests
 import json
 import telebot
@@ -5,16 +6,16 @@ from telebot import types
 url = "https://api.weather.yandex.ru/v2/informers?lat=55.75222&lon=37.61556"
 headers = {"X-Yandex-API-Key": "weather token"}
 bot = telebot.TeleBot('6169575331:AAEBdTW60CMa24zsWcxA_42SAzi6QCC1M3M');
-@bot.message_handler(commands=['button'])
+@bot.message_handler(commands=['start'])
 def button_message(message):
     markup=types.ReplyKeyboardMarkup(resize_keyboard=True)
-    item1=types.KeyboardButton("Кнопка")
+    item1=types.KeyboardButton("Сколько мы вместе❤")
     markup.add(item1)
     bot.send_message(message.chat.id,'Выберите что вам надо',reply_markup=markup)
 @bot.message_handler(content_types='text')
 def message_reply(message):
-    if message.text=="Кнопка":
-        bot.send_message(message.chat.id,"https://habr.com/ru/users/lubaznatel/")
+    if message.text=="Сколько мы вместе❤":
+        bot.send_message(message.chat.id,ourdate())
 @bot.message_handler(commands=['weather'])
 def get_weather(message):
     r = requests.get(url=url, headers=headers)
