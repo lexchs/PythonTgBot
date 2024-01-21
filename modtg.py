1ca5b32c9cbb8e81891454275e65ea5be48f0450
lexchs <124810229+lexchs@users.noreply.github.com>
2024-01-21 18:36:19 +0300

    Update modtg.py

--- modtg.py
+++ modtg.py
@@ -1,12 +1,13 @@
-from datetime import datetime
-from datetime import date
-ourday = datetime(2020,1,25,23,59,59)
-today = datetime.now()
-diff=ourday-today
-ydiff=diff.days*-1
-hdiff=diff.seconds//3600
-mdiff=(diff.seconds%3600)//60
-sdiff=(diff.seconds%3600)%60
-
-
-print(ydiff,hdiff,mdiff,sdiff)
+def ourdate():
+	from datetime import datetime
+	from datetime import date
+	ourday = datetime(2020,1,25,23,59,59)
+	today = datetime.now()
+	diff=today-ourday
+	ydiff=diff.days
+	hdiff=diff.seconds//3600
+	mdiff=(diff.seconds%3600)//60
+	sdiff=(diff.seconds%3600)%60
+	x=f"Мы вместе: {ydiff}-дней, {hdiff}-часов,{mdiff}-минут, {sdiff}-секунд!❤"
+	return x
+print(ourdate())
